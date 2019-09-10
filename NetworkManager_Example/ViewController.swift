7a1826106c4035c59e7775a158e4dbaa8e6e821c
Senchik Dima <dmitriy.senchik@gmail.com>
2019-09-10 23:09:09 +0200

    Initial Commit

---
+++ NetworkManager_Example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NetworkManager_Example
+//
+//  Created by Dima Senchik on 9/10/19.
+//  Copyright © 2019 Dima Senchik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
